b59842dc5166235709c551882c0e5b8fb10dc449
luca01github <luca.porcheddu01@gmail.com>
2025-05-20 00:34:17 +0200

    Update statistics.h

--- statistics.h
+++ statistics.h
@@ -14,20 +14,4 @@ float calcDevStandardF(float array[], int size);
 float calcDevStandardI(int array[], int size);
 float calcDevStandardD(double array[], int size);
 
-float calcAsimmetriaF(float array[], int size);
-float calcAsimmetriaI(int array[], int size);
-float calcAsimmetriaD(double array[], int size);
-
-float calcCurtosiF(float array[], int size);
-float calcCurtosiI(int array[], int size);
-float calcCurtosiD(double array[], int size);
-
-float calcCovarianceF(float a[], float b[], int size);
-float calcCovarianceI(int a[], int b[], int size);
-float calcCovarianceD(double a[], double b[], int size);
-
-float calcCoeffCorrelationF(float a[], float b[], int size);
-float calcCoeffCorrelationI(int a[], int b[], int size);
-float calcCoeffCorrelationD(double a[], double b[], int size);
-
 #endif // STATS_H
